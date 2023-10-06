d5a8632cf6e8b25dc6a90547163fa87588816147
Dương Anh Phụng <duongphung16296@gmail.com>
2023-10-06 09:09:12 +0700

    update UI

--- components/MultipleItemsCarousel/index.tsx
+++ components/MultipleItemsCarousel/index.tsx
@@ -15,7 +15,7 @@ const MultipleItemsCarousel = (props: MultipleItemsCarouselProps) => {
   const settings = {
     dots: false,
     infinite: true,
-    speed: 500,
+    speed: 300,
     slidesToShow: sources.length > 5 ? 5 : sources.length, // Number of items to show at a time
     slidesToScroll: 1,
     autoplay: true,
@@ -40,7 +40,8 @@ const MultipleItemsCarousel = (props: MultipleItemsCarouselProps) => {
     <Slider {...settings}>
       {sources?.map((item, index) => (
         <div key={index}>
-          <div style={{ flexDirection: 'column' }} className='flex items-center'>
+          <div style={{ flexDirection: 'column' }}
+            className='py-5 flex items-center hover:text-primary cursor-pointer transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-125 hover:bg-indigo-500 duration-300 ...' >
             <img src={item.image} alt={`image_${index}`} style={{ width: 50, height: 50 }} />
             <div className="mt-2">{item.name}</div>
           </div>
